3f80cb56504937ecb17f375a0f5c46cf904be933
Tridro <tridro@beneorigin.com>
2023-03-28 11:51:06 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time         : 2023/2/14 10:04
+# @Author       : Tridro
+# @Email        : tridro@beneorigin.com
+# @Project      : chatgpt-on-wxwork
+# @File         : __init__.py
+# @Software     : PyCharm
+# All Copyright Reserved
